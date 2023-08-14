3fb2e56bffa5b34e6a29f5835f89857bf96c6447
juana <juanakoczy@gmail.com>
2023-08-14 19:35:42 -0300

    cola circular

--- cmake-build-debug/colacircular.h
+++ cmake-build-debug/colacircular.h
@@ -14,5 +14,14 @@ typedef struct cola{
 }Cola;
 
 void iniciarCola (Cola *cola);
+int colaVacia (Cola * cola); //verifica si la cola esta vacia
+int colallena (Cola * cola); //verifica si la cola esta llena
+
+void encolar (Cola * cola, int valor); //insertar un valor en la cola
+int Desencolar (Cola * cola, int valor);//elimina un elemento de la cola
+
+int front (Cola * cola); //Obtener el primer elemento sin eliminarlo
+int rear (Cola * cola);//Obtener el ultimo elemento sin eliminarlo
 
 #endif //COLA_CIRCULAR_COLACIRCULAR_H
+
--- main.c
+++ main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
+#include "cmake-build-debug/colacircular.h"
+
 
 int main() {
-    printf("Hello, World!\n");
+    Cola *cola;
+    iniciarCola(cola);
+
+    encolar(cola,10);
+    encolar(cola,20);
+    encolar(cola,30);
+    encolar(cola,40);
+    encolar(cola,50);
+
+    printf ("Primer elemento: %d", front(cola));
+
+    printf ("Desencolar %d", Desencolar(cola,10));
+
+    printf ("Primer elemento: %d", front(cola));
+
+    encolar(cola,60);
+    encolar(cola,70);
+
+
+
     return 0;
 }
